8259216c9d18cc7815ce6adcf233d4b44835c590
jarryliu <jarrysix@gmail.com>
2016-05-20 22:17:00 +0800

    make.sh

--- make.sh
+++ make.sh
@@ -2,5 +2,5 @@ CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o godns-amd64
 CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -o godns-i386
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o godns-mac
 CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o godns-win
-
+CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -o godns-arm
 echo 'compile success!'
