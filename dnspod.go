f81d1c974af2f5e5d52ac0e973a84b42b596ea21
jarryliu <jarrysix@gmail.com>
2016-05-20 14:21:56 +0800

    fmt

--- dnspod.go
+++ dnspod.go
@@ -1,223 +1,222 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "log"
-    "net/http"
-    "net/url"
-    "strconv"
-    "strings"
-    "github.com/bitly/go-simplejson"
-    "time"
-    "os"
-    "errors"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/bitly/go-simplejson"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 const (
-    ApiServer = "https://dnsapi.cn" //必须使用https
-    AuthorEmail = "jarrysix@gmail.com"
+	ApiServer   = "https://dnsapi.cn" //必须使用https
+	AuthorEmail = "jarrysix@gmail.com"
 )
 
 var (
-    loginToken string
+	loginToken string
 )
 
 // 获取接口登陆Token
 func getLoginToken() string {
-    if (loginToken == "") {
-        loginToken = fmt.Sprintf("%d,%s", Configuration.ApiId, Configuration.ApiToken)
-    }
-    return loginToken
+	if loginToken == "" {
+		loginToken = fmt.Sprintf("%d,%s", Configuration.ApiId, Configuration.ApiToken)
+	}
+	return loginToken
 }
 
-func get_currentIP(url string) (string, error) {
-    response, err := http.Get(url)
-
-    if err != nil {
-        log.Println("Cannot get IP...")
-        return "", err
-    }
-
-    defer response.Body.Close()
-
-    body, _ := ioutil.ReadAll(response.Body)
-    return string(body), nil
+func getExternalIp(url string) (string, error) {
+	//http://myexternalip.com/raw
+	//http://members.3322.org/dyndns/getip
+	response, err := http.Get(url)
+	if err != nil {
+		log.Println("Cannot get IP...")
+		return "", err
+	}
+	defer response.Body.Close()
+	body, _ := ioutil.ReadAll(response.Body)
+	return string(body), nil
 }
 
 // 附加公共API参数
 func appendCommParams(params url.Values) url.Values {
-    if params == nil {
-        panic("params not be nil!")
-    }
-    //header.Add("login_email", Configuration.Email)
-    //header.Add("login_password", Configuration.Password)
-    params.Set("format", "json")
-    params.Set("lang", "en")
-    params.Set("error_on_empty", "no")
-    params.Set("login_token", getLoginToken())
-    //仅代理用户需要使用以下参数
-    //params.Set("user_id","")
-    return params
+	if params == nil {
+		panic("params not be nil!")
+	}
+	//header.Add("login_email", Configuration.Email)
+	//header.Add("login_password", Configuration.Password)
+	params.Set("format", "json")
+	params.Set("lang", "en")
+	params.Set("error_on_empty", "no")
+	params.Set("login_token", getLoginToken())
+	//仅代理用户需要使用以下参数
+	//params.Set("user_id","")
+	return params
 }
 
 // 获取版本
 func GetApiVersion() *Version {
-    ver := map[string]interface{}{}
-    b, err := apiPost("/Info.Version", url.Values{})
-    if err == nil {
-        json.Unmarshal(b, &ver)
-    }
-    if err != nil {
-        panic(err)
-    }
+	ver := map[string]interface{}{}
+	b, err := apiPost("/Info.Version", url.Values{})
+	if err == nil {
+		json.Unmarshal(b, &ver)
+	}
+	if err != nil {
+		panic(err)
+	}
 
-    stat := ver["status"].(map[string]interface{})
-    code := stat["code"]
-    msg := stat["message"]
-    date, _ := time.Parse("2006-01-02 15:04:05", stat["created_at"].(string))
+	stat := ver["status"].(map[string]interface{})
+	code := stat["code"]
+	msg := stat["message"]
+	date, _ := time.Parse("2006-01-02 15:04:05", stat["created_at"].(string))
 
-    log.Println(code, getLoginToken())
-    if code != "1" {
-        log.Println(fmt.Sprintf("[ Check][ Version] - code:%s,message:%s", code, msg))
-        os.Exit(0)
-    }
+	log.Println(code, getLoginToken())
+	if code != "1" {
+		log.Println(fmt.Sprintf("[ Check][ Version] - code:%s,message:%s", code, msg))
+		os.Exit(0)
+	}
 
-    return &Version{
-        ApiVersion: msg.(string),
-        ApiDate: date,
-        ClientVersion:ClientVersion,
-    }
+	return &Version{
+		ApiVersion:    msg.(string),
+		ApiDate:       date,
+		ClientVersion: ClientVersion,
+	}
 }
 
 func get_domain(name string) int64 {
-    var ret int64
-    values := url.Values{}
-    values.Add("type", "all")
-    values.Add("offset", "0")
-    values.Add("length", "20")
+	var ret int64
+	values := url.Values{}
+	values.Add("type", "all")
+	values.Add("offset", "0")
+	values.Add("length", "20")
 
-    response, err := apiPost("/Domain.List", values)
+	response, err := apiPost("/Domain.List", values)
 
-    if err != nil {
-        log.Println("Failed to get domain list...")
-        return -1
-    }
+	if err != nil {
+		log.Println("Failed to get domain list...")
+		return -1
+	}
 
-    sjson, parse_err := simplejson.NewJson([]byte(response))
+	sjson, parse_err := simplejson.NewJson([]byte(response))
 
-    if parse_err != nil {
-        log.Println(parse_err)
-        return -1
-    }
+	if parse_err != nil {
+		log.Println(parse_err)
+		return -1
+	}
 
-    if sjson.Get("status").Get("code").MustString() == "1" {
-        domains, _ := sjson.Get("domains").Array()
+	if sjson.Get("status").Get("code").MustString() == "1" {
+		domains, _ := sjson.Get("domains").Array()
 
-        for _, d := range domains {
-            m := d.(map[string]interface{})
-            if m["name"] == name {
-                id := m["id"]
+		for _, d := range domains {
+			m := d.(map[string]interface{})
+			if m["name"] == name {
+				id := m["id"]
 
-                switch t := id.(type) {
-                case json.Number:
-                    ret, _ = t.Int64()
-                }
+				switch t := id.(type) {
+				case json.Number:
+					ret, _ = t.Int64()
+				}
 
-                break
-            }
-        }
-        if len(domains) == 0 {
-            log.Println("domains slice is empty.")
-        }
-    } else {
-        log.Println("get_domain:status code:", sjson.Get("status").Get("code").MustString())
-    }
+				break
+			}
+		}
+		if len(domains) == 0 {
+			log.Println("domains slice is empty.")
+		}
+	} else {
+		log.Println("get_domain:status code:", sjson.Get("status").Get("code").MustString())
+	}
 
-    return ret
+	return ret
 }
 
 func getSubdomain(domain_id int64, name string) (string, string) {
-    var ret, ip string
-    value := url.Values{}
-    value.Add("domain_id", strconv.FormatInt(domain_id, 10))
-    value.Add("offset", "0")
-    value.Add("length", "1")
-    value.Add("sub_domain", name)
+	var ret, ip string
+	value := url.Values{}
+	value.Add("domain_id", strconv.FormatInt(domain_id, 10))
+	value.Add("offset", "0")
+	value.Add("length", "1")
+	value.Add("sub_domain", name)
 
-    response, err := apiPost("/Record.List", value)
+	response, err := apiPost("/Record.List", value)
 
-    if err != nil {
-        log.Println("Failed to get domain list")
-        return "", ""
-    }
+	if err != nil {
+		log.Println("Failed to get domain list")
+		return "", ""
+	}
 
-    sjson, parse_err := simplejson.NewJson([]byte(response))
+	sjson, parse_err := simplejson.NewJson([]byte(response))
 
-    if parse_err != nil {
-        log.Println(parse_err)
-        return "", ""
-    }
+	if parse_err != nil {
+		log.Println(parse_err)
+		return "", ""
+	}
 
-    if sjson.Get("status").Get("code").MustString() == "1" {
-        records, _ := sjson.Get("records").Array()
+	if sjson.Get("status").Get("code").MustString() == "1" {
+		records, _ := sjson.Get("records").Array()
 
-        for _, d := range records {
-            m := d.(map[string]interface{})
-            if m["name"] == name {
-                ret = m["id"].(string)
-                ip = m["value"].(string)
-                break
-            }
-        }
-        if len(records) == 0 {
-            log.Println("records slice is empty.")
-        }
-    } else {
-        log.Println("get_subdomain:status code:", sjson.Get("status").Get("code").MustString())
-    }
+		for _, d := range records {
+			m := d.(map[string]interface{})
+			if m["name"] == name {
+				ret = m["id"].(string)
+				ip = m["value"].(string)
+				break
+			}
+		}
+		if len(records) == 0 {
+			log.Println("records slice is empty.")
+		}
+	} else {
+		log.Println("get_subdomain:status code:", sjson.Get("status").Get("code").MustString())
+	}
 
-    return ret, ip
+	return ret, ip
 }
 
 // 更新IP记录
 func UpdateIpRecord(domainId int64, subDomainId string, subDomainName string, ip string) error {
-    value := url.Values{}
-    value.Add("domain_id", strconv.FormatInt(domainId, 10))
-    value.Add("record_id", subDomainId)
-    value.Add("sub_domain", subDomainName)
-    value.Add("record_type", "A")
-    value.Add("record_line", "默认")
-    value.Add("value", ip)
+	value := url.Values{}
+	value.Add("domain_id", strconv.FormatInt(domainId, 10))
+	value.Add("record_id", subDomainId)
+	value.Add("sub_domain", subDomainName)
+	value.Add("record_type", "A")
+	value.Add("record_line", "默认")
+	value.Add("value", ip)
 
-    response, err := apiPost("/Record.Modify", value)
+	response, err := apiPost("/Record.Modify", value)
 
-    if err == nil {
-        sjson, _ := simplejson.NewJson([]byte(response))
-        code := sjson.Get("status").Get("code").MustString()
-        // 更新地址成功
-        if code == "1" {
-            return nil
-        }
-        return errors.New(fmt.Sprintf("record update fail!(code:%s)", code))
-    }
-    return err
+	if err == nil {
+		sjson, _ := simplejson.NewJson([]byte(response))
+		code := sjson.Get("status").Get("code").MustString()
+		// 更新地址成功
+		if code == "1" {
+			return nil
+		}
+		return errors.New(fmt.Sprintf("record update fail!(code:%s)", code))
+	}
+	return err
 }
 
 // 提交数据到接口
 func apiPost(url string, content url.Values) ([]byte, error) {
-    client := http.DefaultClient
-    values := appendCommParams(content)
-    req, _ := http.NewRequest("POST", ApiServer + url, strings.NewReader(values.Encode()))
-    req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-    req.Header.Set("User-Agent", fmt.Sprintf("GoDNS/0.1 (%s)", AuthorEmail))
-    response, err := client.Do(req)
-    defer response.Body.Close()
+	client := http.DefaultClient
+	values := appendCommParams(content)
+	req, _ := http.NewRequest("POST", ApiServer+url, strings.NewReader(values.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("User-Agent", fmt.Sprintf("GoDNS/0.1 (%s)", AuthorEmail))
+	response, err := client.Do(req)
+	defer response.Body.Close()
 
-    if err != nil {
-        log.Println("Post failed...", err.Error())
-        return nil, err
-    }
-    return ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Println("Post failed...", err.Error())
+		return nil, err
+	}
+	return ioutil.ReadAll(response.Body)
 }
--- godns.go
+++ godns.go
@@ -1,154 +1,155 @@
 package main
 
 import (
-    "flag"
-    "log"
-    "runtime/debug"
-    "strings"
-    "time"
-    "io/ioutil"
-    "os"
-    "encoding/json"
+	"encoding/json"
+	"flag"
+	"io/ioutil"
+	"log"
+	"os"
+	"runtime/debug"
+	"strings"
+	"time"
 )
 
 const (
-    PANIC_MAX = 5
-    DnsUpdateInterval = 5 * time.Minute //Minute
-    ClientVersion = "0.1" //客户端版本
+	PANIC_MAX         = 5
+	DnsUpdateInterval = 5 * time.Minute //Minute
+	ClientVersion     = "0.1"           //客户端版本
 )
 
 var (
-    Configuration *Settings
-    latestIp string    // 上次的IP地址
-    optConf = flag.String("c", "./godns.conf", "config file")
-    optCommand = flag.String("s", "", "send signal to a master process: stop, quit, reopen, reload")
-    optHelp = flag.Bool("h", false, "this help")
-    panicCount = 0
-    DomainId int64
-    SubDomainArr = []string{}
-    SubDomainIdArr = []string{}
+	Configuration  *Settings
+	latestIp       string // 上次的IP地址
+	optConf        = flag.String("c", "./godns.conf", "config file")
+	optCommand     = flag.String("s", "", "send signal to a master process: stop, quit, reopen, reload")
+	optHelp        = flag.Bool("h", false, "this help")
+	panicCount     = 0
+	DomainId       int64
+	SubDomainArr   = []string{}
+	SubDomainIdArr = []string{}
 )
 
-type(
-    // 版本
-    Version struct {
-        ApiVersion    string
-        ApiDate       time.Time
-        ClientVersion string
-    }
-    Settings struct {
-        Email      string
-        Domain     string
-        ApiId      int      `json:"api_id"`
-        ApiToken   string  `json:"api_token"`
-        Sub_domain string
-        IP_Url     string
-        Log_Path   string
-        Log_Size   int
-        Log_Num    int
-        User       int
-        Group      int
-    }
+type (
+	// 版本
+	Version struct {
+		ApiVersion    string
+		ApiDate       time.Time
+		ClientVersion string
+	}
+	Settings struct {
+		Email      string
+		Domain     string
+		ApiId      int    `json:"api_id"`
+		ApiToken   string `json:"api_token"`
+		Sub_domain string
+		IP_Url     string
+		Log_Path   string
+		Log_Size   int
+		Log_Num    int
+		User       int
+		Group      int
+	}
 )
 
 func LoadSettings(config_path string) *Settings {
-    file, err := ioutil.ReadFile(config_path)
-    if err != nil {
-        log.Println("Error occurs while reading config file, please make sure config file exists!")
-        os.Exit(1)
-    }
+	file, err := ioutil.ReadFile(config_path)
+	if err != nil {
+		log.Println("Error occurs while reading config file, please make sure config file exists!")
+		os.Exit(1)
+	}
 
-    var setting Settings
-    err = json.Unmarshal(file, &setting)
-    if err != nil {
-        log.Println("Error occurs while unmarshal config file, please make sure config file correct!")
-        os.Exit(1)
-    }
-    return &setting
+	var setting Settings
+	err = json.Unmarshal(file, &setting)
+	if err != nil {
+		log.Println("Error occurs while unmarshal config file, please make sure config file correct!")
+		os.Exit(1)
+	}
+	return &setting
 }
 
 func usage() {
-    log.Println("[command] -c=[config file path]")
-    flag.PrintDefaults()
+	log.Println("[command] -c=[config file path]")
+	flag.PrintDefaults()
 }
 func main() {
-    flag.Parse()
-    if *optHelp {
-        usage()
-        return
-    }
-    log.SetFlags(log.Lshortfile | log.Ltime | log.LstdFlags)
+	flag.Parse()
+	if *optHelp {
+		usage()
+		return
+	}
+	log.SetFlags(log.Lshortfile | log.Ltime | log.LstdFlags)
 
-    Configuration = LoadSettings(*optConf)
+	Configuration = LoadSettings(*optConf)
 
-    ver := GetApiVersion()
-    log.Println("[ GoDns][ Version] -", " latest :", ver.ApiVersion,
-        " release :", ver.ApiDate.Format("2006-01-02"))
-    checkDomain()
-    dnsUpdateLoop()
+	ver := GetApiVersion()
+	log.Println("[ GoDns][ Version] -", " latest :", ver.ApiVersion,
+		" release :", ver.ApiDate.Format("2006-01-02"))
+	checkDomain()
+	dnsUpdateLoop()
 }
 
 // 检测域名
 func checkDomain() {
-    domain := strings.TrimSpace(Configuration.Domain)
-    DomainId = get_domain(domain)
-    if DomainId == -1 {
-        log.Println("[ GoDns][ Error] - domain :", domain, " dont't be resolve by DnsPod.")
-        os.Exit(0)
-    }
+	domain := strings.TrimSpace(Configuration.Domain)
+	DomainId = get_domain(domain)
+	if DomainId == -1 {
+		log.Println("[ GoDns][ Error] - domain :", domain, " dont't be resolve by DnsPod.")
+		os.Exit(0)
+	}
 
-    SubDomainArr = strings.Split(Configuration.Sub_domain, ",")
-    for i, v := range SubDomainArr {
-        v = strings.TrimSpace(v)
-        if len(v) != 0 {
-            subDomainId, ip := getSubdomain(DomainId, v)
-            subDomain := v + "." + domain
-            if subDomainId == "" || ip == "" {
-                log.Println("[ GoDns][ Wanning] - ", subDomain, " not in list.")
-                SubDomainArr = append(SubDomainArr[:i],SubDomainArr[i+1:]...)
-            } else {
-                log.Println("[ GoDns][ Stat] - ", subDomain, "=>", ip)
-                SubDomainIdArr = append(SubDomainIdArr, subDomainId)
-            }
-        }
-    }
+	SubDomainArr = strings.Split(Configuration.Sub_domain, ",")
+	for i, v := range SubDomainArr {
+		v = strings.TrimSpace(v)
+		if len(v) != 0 {
+			subDomainId, ip := getSubdomain(DomainId, v)
+			subDomain := v + "." + domain
+			if subDomainId == "" || ip == "" {
+				log.Println("[ GoDns][ Wanning] - ", subDomain, " not in list.")
+				SubDomainArr = append(SubDomainArr[:i], SubDomainArr[i+1:]...)
+			} else {
+				log.Println("[ GoDns][ Stat] - ", subDomain, "=>", ip)
+				SubDomainIdArr = append(SubDomainIdArr, subDomainId)
+			}
+		}
+	}
 }
 
 func dnsUpdateLoop() {
-    defer func() {
-        if err := recover(); err != nil {
-            panicCount++
-            log.Printf("Recovered in %v: %v\n", err, debug.Stack())
-            if panicCount < PANIC_MAX {
-                log.Println("Got panic in goroutine, will start a new one... :", panicCount)
-                go dnsUpdateLoop()
-            }
-        }
-    }()
+	defer func() {
+		if err := recover(); err != nil {
+			panicCount++
+			log.Printf("Recovered in %v: %v\n", err, debug.Stack())
+			if panicCount < PANIC_MAX {
+				log.Println("Got panic in goroutine, will start a new one... :", panicCount)
+				go dnsUpdateLoop()
+			}
+		}
+	}()
 
-    for {
-        localIp, err := get_currentIP(Configuration.IP_Url)
-        if err != nil {
-            log.Println("[ GoDns][ Error] - fetch ip error:", err.Error())
-            continue
-        }
-        //检测IP是否有变化,如无变化则不提交更新
-        if localIp == latestIp {
-            log.Println("[ GoDns][ Stat] - ip not need be update!")
-        } else {
-            log.Println("[ GoDns][ Stat] - current ip is ", localIp, ", will be updated!")
-            for i,subId := range SubDomainIdArr {
-                if err = UpdateIpRecord(DomainId, subId, SubDomainArr[i], localIp); err != nil {
-                    log.Println("[ GoDns][ Update]- subdomain ", SubDomainArr[i], err.Error())
-                } else {
-                    log.Println("[ GoDns][ Update]- subdomain ", SubDomainArr[i], " update success!")
-                }
-            }
-        }
+	for {
+		localIp, err := getExternalIp(Configuration.IP_Url)
+		if err != nil {
+			log.Println("[ GoDns][ Error] - fetch ip error:", err.Error())
+			continue
+		}
+		//检测IP是否有变化,如无变化则不提交更新
+		if localIp == latestIp {
+			log.Println("[ GoDns][ Stat] - ip not need be update!")
+		} else {
+			latestIp = localIp
+			log.Println("[ GoDns][ Stat] - current ip is ", localIp, ", will be updated!")
+			for i, subId := range SubDomainIdArr {
+				if err = UpdateIpRecord(DomainId, subId, SubDomainArr[i], localIp); err != nil {
+					log.Println("[ GoDns][ Update]- subdomain ", SubDomainArr[i], err.Error())
+				} else {
+					log.Println("[ GoDns][ Update]- subdomain ", SubDomainArr[i], " update success!")
+				}
+			}
+		}
 
-        //Interval is 5 minutes
-        time.Sleep(DnsUpdateInterval)
-    }
+		//Interval is 5 minutes
+		time.Sleep(DnsUpdateInterval)
+	}
 
-    log.Printf("Loop %d exited...\n", panicCount)
+	log.Printf("Loop %d exited...\n", panicCount)
 }
