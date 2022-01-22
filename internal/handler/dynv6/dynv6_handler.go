05035a19191ee02c932f0397d49ffda92efa69b5
apatura-iris <apatura-iris@yb171.org>
2022-01-22 12:26:10 -0700

    put update of ip address in function to make the cleanup with defer meaningful

--- internal/handler/dynv6/dynv6_handler.go
+++ internal/handler/dynv6/dynv6_handler.go
@@ -3,6 +3,7 @@ package dynv6
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"runtime/debug"
 	"strings"
 	"time"
@@ -57,13 +58,6 @@ func (handler *Handler) DomainLoop(domain *settings.Domain, panicChan chan<- set
 
 		log.Debug("currentIP is:", currentIP)
 		client := utils.GetHttpClient(handler.Configuration, handler.Configuration.UseProxy)
-		var ip string
-
-		if strings.ToUpper(handler.Configuration.IPType) == utils.IPV4 {
-			ip = fmt.Sprintf("ipv4=%s", currentIP)
-		} else if strings.ToUpper(handler.Configuration.IPType) == utils.IPV6 {
-			ip = fmt.Sprintf("ipv6=%s", currentIP)
-		}
 
 		for _, subDomain := range domain.SubDomains {
 			hostname := subDomain + "." + domain.DomainName
@@ -77,33 +71,47 @@ func (handler *Handler) DomainLoop(domain *settings.Domain, panicChan chan<- set
 			//check against currently known IP, if no change, skip update
 			if currentIP == lastIP {
 				log.Infof("IP is the same as cached one (%s). Skip update.\n", currentIP)
+				notify.GetNotifyManager(handler.Configuration).Send(hostname, currentIP)
 			} else {
-				// update IP with HTTP GET request
-				url := fmt.Sprintf(Dynv6Url, hostname, handler.Configuration.LoginToken, ip)
-				log.Debug("Update url: ", url)
-				resp, err := client.Get(url)
+				err := handler.update(client, hostname, currentIP)
 				if err != nil {
-					// handle error
 					log.Errorf("Failed to update domain %s: %v", hostname, err)
-					continue
-				}
-
-				defer resp.Body.Close()
-
-				body, err := ioutil.ReadAll(resp.Body)
-				if err != nil {
-					log.Error("Failed to update the IP: ", err)
-					continue
-				} else if string(body) != "OK" {
-					log.Error("Failed to update the IP: ", string(body))
-					continue
 				} else {
 					log.Info("IP updated to:", currentIP)
 				}
-
-				// Send notification
-				notify.GetNotifyManager(handler.Configuration).Send(fmt.Sprintf("%s.%s", subDomain, domain.DomainName), currentIP)
 			}
 		}
 	}
+}
+
+func (handler *Handler) update(client *http.Client, hostname string, currentIP string) error {
+
+	var ip string
+	if strings.ToUpper(handler.Configuration.IPType) == utils.IPV4 {
+		ip = fmt.Sprintf("ipv4=%s", currentIP)
+	} else if strings.ToUpper(handler.Configuration.IPType) == utils.IPV6 {
+		ip = fmt.Sprintf("ipv6=%s", currentIP)
+	}
+
+	// update IP with HTTP GET request
+	url := fmt.Sprintf(Dynv6Url, hostname, handler.Configuration.LoginToken, ip)
+	log.Debug("Update url: ", url)
+	resp, err := client.Get(url)
+	if err != nil {
+		// handle error
+		return fmt.Errorf("cannot send request: %w", err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to receive response: %w", err)
+	} else if !strings.Contains(string(body), "addresses updated") {
+		return fmt.Errorf("service rejected update: %s", string(body))
+	}
+
+	// Send notification
+	notify.GetNotifyManager(handler.Configuration).Send(hostname, currentIP)
+	return nil
 }
