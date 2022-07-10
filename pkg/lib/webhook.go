4fb7c460f2dda5f1ce841522a1755d468aa5f2c8
Timothy <yexiaozhou2003@gmail.com>
2022-07-10 14:06:05 +0800

    add webhook test case

--- pkg/lib/webhook.go
+++ pkg/lib/webhook.go
@@ -1,12 +1,14 @@
 package lib
 
 import (
+	"bytes"
 	"net/http"
 	"sync"
-
-	"github.com/TimothyYe/godns/internal/utils"
+	"text/template"
 
 	"github.com/TimothyYe/godns/internal/settings"
+	"github.com/TimothyYe/godns/internal/utils"
+	log "github.com/sirupsen/logrus"
 )
 
 type Webhook struct {
@@ -31,5 +33,40 @@ func GetWebhook(conf *settings.Settings) *Webhook {
 }
 
 func (w *Webhook) Execute(domain, currentIP string) error {
+	if w.conf.Webhook.URL == "" {
+		log.Debug("Webhook URL is empty, skip sending notification")
+		return nil
+	}
+
 	return nil
+}
+
+func (w *Webhook) buildReqURL(domain, currentIP, ipType string) (string, error) {
+	t := template.New("notification template")
+	if _, err := t.Parse(w.conf.Webhook.URL); err != nil {
+		log.Error("Failed to parse template:", err)
+		return "", err
+	}
+
+	data := struct {
+		CurrentIP string
+		Domain    string
+		IPType    string
+	}{
+		currentIP,
+		domain,
+		ipType,
+	}
+
+	var tpl bytes.Buffer
+	if err := t.Execute(&tpl, data); err != nil {
+		log.Error(err)
+		return "", err
+	}
+
+	return tpl.String(), nil
+}
+
+func (w *Webhook) buildReqBody() (string, error) {
+	return "", nil
 }
---
+++ pkg/lib/webhook_test.go
@@ -0,0 +1,27 @@
+package lib
+
+import (
+	"testing"
+
+	"github.com/TimothyYe/godns/internal/settings"
+	"github.com/TimothyYe/godns/internal/utils"
+)
+
+func TestBuildReqURL(t *testing.T) {
+	w := GetWebhook(&settings.Settings{
+		Webhook: settings.Webhook{
+			URL: "http://localhost:5000/api/v1/send?domain={{.Domain}}&ip={{.CurrentIP}}&ip_type={{.IPType}}",
+		}})
+
+	ret, err := w.buildReqURL("example.com", "192.168.1.1", utils.IPV4)
+	if err != nil {
+		t.Error(err)
+	}
+
+	expected := "http://localhost:5000/api/v1/send?domain=example.com&ip=192.168.1.1&ip_type=IPV4"
+	if ret != expected {
+		t.Errorf("expected %s, got %s", expected, ret)
+	}
+
+	t.Log(ret)
+}
