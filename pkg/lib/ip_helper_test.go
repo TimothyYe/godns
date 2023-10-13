30ba7ccf5a903e925e5be797a72ed56c54e8be46
Timothy <yexiaozhou2003@gmail.com>
2023-10-13 22:45:50 +0800

    fix unit test

--- pkg/lib/ip_helper_test.go
+++ pkg/lib/ip_helper_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestGetCurrentIP(t *testing.T) {
-	conf := &settings.Settings{IPUrls: []string{"https://myip.biturl.top", "https://ip4.seeip.org"}}
+	conf := &settings.Settings{IPUrls: []string{"https://myip.biturl.top"}}
 	helper := NewIPHelper(conf)
 	ip := helper.GetCurrentIP()
 
