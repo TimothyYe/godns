5ff78ecd095da246010b444e9ae9282b83484034
Timothy <yexiaozhou2003@gmail.com>
2024-02-07 00:37:09 +0800

    update navbar

--- web/components/navbar.tsx
+++ web/components/navbar.tsx
@@ -17,7 +17,6 @@ import {
 	HeartFilledIcon,
 } from "@/components/icons";
 
-import { Logo } from "@/components/icons";
 
 export const Navbar = () => {
 	return (
@@ -25,8 +24,7 @@ export const Navbar = () => {
 			<NavbarContent className="basis-1/5 sm:basis-full" justify="start">
 				<NavbarBrand as="li" className="gap-3 max-w-fit">
 					<NextLink className="flex justify-start items-center gap-1" href="/">
-						<Logo />
-						<p className="font-bold text-inherit">ACME</p>
+						<p className="font-bold text-inherit">GoDNS</p>
 					</NextLink>
 				</NavbarBrand>
 				<ul className="hidden lg:flex gap-4 justify-start ml-2">
