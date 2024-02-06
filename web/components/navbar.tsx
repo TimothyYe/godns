de6927349e0500dcddf438b8960ddd43f2cd0755
Timothy <yexiaozhou2003@gmail.com>
2024-02-07 00:25:43 +0800

    add web project

---
+++ web/components/navbar.tsx
@@ -0,0 +1,73 @@
+import {
+	Navbar as NextUINavbar,
+	NavbarContent,
+	NavbarBrand,
+	NavbarItem,
+} from "@nextui-org/navbar";
+import { Link } from "@nextui-org/link";
+import { link as linkStyles } from "@nextui-org/theme";
+
+import { siteConfig } from "@/config/site";
+import NextLink from "next/link";
+import clsx from "clsx";
+
+import { ThemeSwitch } from "@/components/theme-switch";
+import {
+	GithubIcon,
+	HeartFilledIcon,
+} from "@/components/icons";
+
+import { Logo } from "@/components/icons";
+
+export const Navbar = () => {
+	return (
+		<NextUINavbar maxWidth="xl" position="sticky">
+			<NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+				<NavbarBrand as="li" className="gap-3 max-w-fit">
+					<NextLink className="flex justify-start items-center gap-1" href="/">
+						<Logo />
+						<p className="font-bold text-inherit">ACME</p>
+					</NextLink>
+				</NavbarBrand>
+				<ul className="hidden lg:flex gap-4 justify-start ml-2">
+					{siteConfig.navItems.map((item) => (
+						<NavbarItem key={item.href}>
+							<NextLink
+								className={clsx(
+									linkStyles({ color: "foreground" }),
+									"data-[active=true]:text-primary data-[active=true]:font-medium"
+								)}
+								color="foreground"
+								href={item.href}
+							>
+								{item.label}
+							</NextLink>
+						</NavbarItem>
+					))}
+				</ul>
+			</NavbarContent>
+
+			<NavbarContent
+				className="hidden sm:flex basis-1/5 sm:basis-full"
+				justify="end"
+			>
+				<NavbarItem className="hidden sm:flex gap-2">
+					<Link isExternal href={siteConfig.links.github} aria-label="Github">
+						<GithubIcon className="text-default-500" />
+					</Link>
+					<Link isExternal href={siteConfig.links.sponsor} aria-label="Sponsor">
+						<HeartFilledIcon className="text-danger" />
+					</Link>
+					<ThemeSwitch />
+				</NavbarItem>
+			</NavbarContent>
+
+			<NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+				<Link isExternal href={siteConfig.links.github} aria-label="Github">
+					<GithubIcon className="text-default-500" />
+				</Link>
+				<ThemeSwitch />
+			</NavbarContent>
+		</NextUINavbar>
+	);
+};
---
+++ web/config/site.ts
@@ -0,0 +1,54 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+	name: "Next.js + NextUI",
+	description: "Make beautiful websites regardless of your design experience.",
+	navItems: [
+		{
+			label: "Home",
+			href: "/",
+		},
+    {
+      label: "About",
+      href: "/about",
+    }
+	],
+	navMenuItems: [
+		{
+			label: "Profile",
+			href: "/profile",
+		},
+		{
+			label: "Dashboard",
+			href: "/dashboard",
+		},
+		{
+			label: "Projects",
+			href: "/projects",
+		},
+		{
+			label: "Team",
+			href: "/team",
+		},
+		{
+			label: "Calendar",
+			href: "/calendar",
+		},
+		{
+			label: "Settings",
+			href: "/settings",
+		},
+		{
+			label: "Help & Feedback",
+			href: "/help-feedback",
+		},
+		{
+			label: "Logout",
+			href: "/logout",
+		},
+	],
+	links: {
+		github: "https://github.com/TimothyYe/godns",
+    sponsor: "https://github.com/sponsors/TimothyYe"
+	},
+};
