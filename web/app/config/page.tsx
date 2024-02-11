e8a675f75ec5668d979381257e49b21a09f573b0
Timothy <yexiaozhou2003@gmail.com>
2024-02-11 15:12:18 +0800

    remove web

--- web/app/config/page.tsx
+++
@@ -1,9 +0,0 @@
-import { subtitle } from "@/components/primitives";
-
-export default function AboutPage() {
-	return (
-		<div>
-			<h2 className={subtitle()}>Config</h2>
-		</div>
-	);
-}
--- web/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  distDir: "dist",
-};
-
-module.exports = nextConfig;
