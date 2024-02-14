f538ac0dbccc780d8c8b995d29588d2d63204685
Timothy <yexiaozhou2003@gmail.com>
2024-02-14 21:57:33 +0800

    update navbar

--- web/components/navbar.tsx
+++ web/components/navbar.tsx
@@ -56,10 +56,10 @@ export const Navbar = () => {
 					<input type="checkbox" value="dark" className="toggle theme-controller" />
 					<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
 				</label>
-				<a className="hidden sm:flex link" href={siteConfig.links.github} aria-label="Github">
+				<a className="hidden sm:flex link" href={siteConfig.links.github} target="_blank" aria-label="Github">
 					<GithubIcon className="text-default-500" />
 				</a>
-				<a className="hidden sm:flex link" href={siteConfig.links.sponsor} aria-label="Sponsor">
+				<a className="hidden sm:flex link" href={siteConfig.links.sponsor} target="_blank" aria-label="Sponsor">
 					<HeartFilledIcon className="text-red-500" />
 				</a>
 				{
