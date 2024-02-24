5694d78c14a7d47940808aece93c186ab1f73bfb
Timothy <yexiaozhou2003@gmail.com>
2024-02-24 14:24:35 +0800

    add proxy control

--- web/app/network/page.tsx
+++ web/app/network/page.tsx
@@ -1,11 +1,13 @@
 import { IpMode } from "@/components/ip-mode";
+import { Proxy } from "@/components/proxy";
 
 export default function Network() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between pt-10 max-w-screen-xl">
 			<div className="p-10">
-				<div className="flex flex-col max-w-screen-lg">
+				<div className="flex flex-col max-w-screen-lg gap-5">
 					<IpMode />
+					<Proxy />
 				</div>
 			</div>
 		</main>
---
+++ web/components/proxy.tsx
@@ -0,0 +1,23 @@
+import { ComputerIcon } from "./icons"
+export const Proxy = () => {
+	return (
+		<div className="stats shadow bg-primary-content stats-vertical lg:stats-horizontal">
+			<div className="stat gap-2">
+				<div className="stat-title">Proxy Settings</div>
+				<div className="flex flex-col gap-3">
+					<div className="flex flex-row items-center justify-start gap-2">
+						<span className="label-text text-slate-500 ">Enable IPv6</span>
+						<input type="checkbox" className="toggle toggle-primary" checked={false} />
+						<span className="label-text text-slate-500 ">Skip SSL Verify</span>
+						<input type="checkbox" className="toggle toggle-primary" checked={false} />
+					</div>
+
+					<input type="text" className="input input-primary w-full input-disabled" placeholder="Input Proxy: e.g. 127.0.0.1:8080"></input>
+					<div className="flex justify-end">
+						<button className="flex justify-end btn btn-sm btn-primary">Save</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
